5b6c717a11ca27c3a5b60c38f246c17caec758e1
Mariano Moreno Alvariza <mmorenoalvariza@gmail.com>
2023-07-03 20:49:26 -0300

    Shrink description

---
+++ src/components/ProductItem/ProductItem.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+import { ProductVariant } from '../../__generated__/graphql';
+import {
+  Article,
+  Image,
+  ProductLeftWrapper,
+  ProductRightWrapper,
+  StyledP,
+} from './ProductItem.styles';
+type ProductItemType = {
+  productVariant: ProductVariant;
+  handleAdd: (id: string) => void;
+};
+
+const ProductItem: FC<ProductItemType> = ({ productVariant, handleAdd }) => {
+  return (
+    <Article>
+      <ProductLeftWrapper>
+        {productVariant.product.featuredAsset && (
+          <Image
+            src={productVariant.product.featuredAsset.source}
+            alt={`${productVariant.name} picture`}
+          />
+        )}
+      </ProductLeftWrapper>
+      <ProductRightWrapper>
+        <h4>{productVariant.name}</h4>
+        <StyledP>{productVariant.product.description}</StyledP>
+        <p>Price: $ {productVariant.price}</p>
+        <div>
+          <button onClick={() => handleAdd(productVariant.id)}>Buy</button>
+        </div>
+      </ProductRightWrapper>
+    </Article>
+  );
+};
+
+export default ProductItem;
---
+++ src/components/ProductItem/__tests__/ProductItem.test.tsx
@@ -0,0 +1,33 @@
+import userEvent from '@testing-library/user-event';
+import { ProductVariant } from '../../../__generated__/graphql';
+import ProductItem from '../ProductItem';
+import { render, screen, waitFor } from '@testing-library/react';
+
+const productVariant = {
+  id: 99,
+  name: 'Laptop',
+  price: 1000,
+  product: {
+    description: 'Product description',
+    featuredAsset: {
+      source: 'public/logo192.png',
+    },
+  },
+} as unknown as ProductVariant;
+
+describe('ProductItem', () => {
+  const addHandler = jest.fn();
+  render(
+    <ProductItem handleAdd={addHandler} productVariant={productVariant} />
+  );
+
+  it('Renders all data points', async () => {
+    expect(screen.getByText(productVariant.name)).toBeVisible();
+    expect(screen.getByText(productVariant.product.description)).toBeVisible();
+    expect(screen.getByText('Price: $ 1000')).toBeVisible();
+    userEvent.click(screen.getByText('Buy'));
+    await waitFor(() =>
+      expect(addHandler).toHaveBeenCalledWith(productVariant.id)
+    );
+  });
+});
